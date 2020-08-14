6974a154c010c4649fa760a8849232b2b1e11a7d
SugarTiger <1029109173@qq.com>
2020-08-14 14:34:48 +0800

    ADD：阶乘后的零

--- LeetCode/172. 阶乘后的零.js
+++ LeetCode/172. 阶乘后的零.js
@@ -4,31 +4,32 @@
  */
 // 线性时间复杂度
 var trailingZeroes = function (n) {
-  var sum = 1;
   // 计算阶乘
-  // 计算0的数量
+  // 一个2和一个5相乘才有一个0，所以计算2和5的个数就知道尾0有多少个
+  // 5的数量肯定比2的数量多，所以只需要计算5的数量
   var count = 0;
-  var twoCount = 0;
-  var fireCount = 0;
-  for (let i = 1; i <= n; i++) {
+  for (let i = 5; i <= n; i += 5) {
     var num = i;
-    if(num % 2 === 0){
-      while (num % 2 === 0) {
-        num /= 2;
-        twoCount++;
-      }
-      i++;
-    }else if(num % 5 === 0){
-      while (num % 5 === 0) {
-        num /= 5;
-        fireCount++;
-      }
+    while (num % 5 === 0) {
+      num /= 5;
+      count++;
     }
   }
-  return Math.min(twoCount,fireCount);
+  return count;
 };
 
-trailingZeroes(2147483647)
+// 优化时间复杂度
+// 
+var trailingZeroes = function (n) {
+  var zeroCount = 0;
+  var currentMultiple = 5;
+  while (n >= currentMultiple) {
+    zeroCount += (n / currentMultiple);
+    currentMultiple *= 5;
+  }
+  return zeroCount;
+}
+
 trailingZeroes(40)
 trailingZeroes(4)
 trailingZeroes(5)
@@ -36,3 +37,4 @@ trailingZeroes(6)
 trailingZeroes(11)
 trailingZeroes(21)
 trailingZeroes(30)
+trailingZeroes(2147483647)
