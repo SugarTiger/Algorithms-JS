f26c248e289b0f276078adc06ee2335a9c444e4a
SugarTiger <1029109173@qq.com>
2020-11-22 10:33:45 +0800

    ADD：每日一题

--- LeetCode/242.有效的字母异位词.js
+++ LeetCode/242.有效的字母异位词.js
@@ -25,4 +25,28 @@ var isAnagram = function (s, t) {
   }
   return true;
 };
-isAnagram("rat", "car")
+
+var isAnagram = function (s, t) {
+  const sLen = s.length;
+  const tLen = t.length;
+  if (sLen !== tLen) return false;
+  const sMap = new Map();
+  const tMap = new Map();
+  for (let i = 0; i < sLen; i++) {
+    if (!sMap.has(s.charAt(i))) {
+      sMap.set(s.charAt(i), 0)
+    }
+    if (!tMap.has(t.charAt(i))) {
+      tMap.set(t.charAt(i), 0)
+    }
+    sMap.set(s.charAt(i), sMap.get(s.charAt(i)) + 1)
+    tMap.set(t.charAt(i), tMap.get(t.charAt(i)) + 1)
+  }
+  for (let [key, value] of sMap) {
+    if (tMap.get(key) !== value) return false;
+  }
+  return true;
+}
+
+
+isAnagram("a", "b")
