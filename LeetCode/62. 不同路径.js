c287a1772a08fdb11f61d7d3e175dad7ba07d129
SugarTiger <1029109173@qq.com>
2020-12-09 11:32:06 +0800

    ADD：每日一题

--- LeetCode/62. 不同路径.js
+++ LeetCode/62. 不同路径.js
@@ -46,7 +46,33 @@ var uniquePaths = function (m, n) {
     dp2 = dp1
     dp1 = temp;
   }
-   return dp1[n - 1]
+  return dp1[n - 1]
+};
+
+// 再优雅一点
+// 时间复杂度 O(m*n) 空间复杂度O(n)
+var uniquePaths = function (m, n) {
+  if (m === 0 || n === 0) return 0;
+  let dp = [];
+  let pre = Array(n).fill(1);
+  for (let i = 1; i < m; i++) {
+    dp[0] = 1;
+    for (let j = 1; j < n; j++) {
+      dp[j] = dp[j - 1] + pre[j];
+    }
+    pre = dp;
+    dp = [];
+  }
+  return pre[n - 1];
+};
+
+// 组合数学  时间复杂度O(min(n,m)) 空间复杂度 O(1)
+var uniquePaths = function(m, n) {
+  let ans = 1;
+  for (let x = n, y = 1; y < m; ++x, ++y) {
+      ans = Math.floor(ans * x / y);
+  }
+  return ans;
 };
 
 uniquePaths(3, 2)
